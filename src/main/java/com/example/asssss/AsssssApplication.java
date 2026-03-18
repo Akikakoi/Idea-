62917bb545837df5ab15174d035b55cf3bcbf2e3
T318 <1740265207@qq.com>
2026-03-18 19:27:02 +0800

    Initial commit

---
+++ src/main/java/com/example/asssss/AsssssApplication.java
@@ -0,0 +1,13 @@
+package com.example.asssss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsssssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsssssApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/asssss/AsssssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asssss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsssssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
