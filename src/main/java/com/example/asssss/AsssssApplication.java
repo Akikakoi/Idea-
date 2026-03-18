e65cfb1a61f9b1373919f832b681055feab4e649
T318 <1740265207@qq.com>
2026-03-18 21:14:31 +0800

    add web env

--- src/main/java/com/example/asssss/AsssssApplication.java
+++ src/main/java/com/example/asssss/AsssssApplication.java
@@ -8,6 +8,7 @@ public class AsssssApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AsssssApplication.class, args);
+        System.out.println("Hello World");
     }
 
 }
